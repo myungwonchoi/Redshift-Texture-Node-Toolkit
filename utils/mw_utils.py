e76cb1bb6c2f3e30afb45b16b298de6dd3b6d636
Myungwon Choi <overzoo1505@gmail.com>
2026-01-15 01:06:27 +0900

    redshift 관련 함수 및 전역변수 redshift_utils로 이동

--- utils/mw_utils.py
+++ utils/mw_utils.py
@@ -3,29 +3,6 @@ import maxon
 import os
 import re
 
-def remove_connections(node, port_id):
-    """
-    특정 노드의 특정 포트에 연결된 모든 연결을 제거합니다.
-    """
-    if not node or not node.IsValid():
-        return
-
-    input_ports = node.GetInputs().GetChildren() # 입력 포트 리스트
-    for input_port in input_ports:
-        port_name = input_port.GetId().ToString()
-        if port_name == port_id:
-            # 각 포트에 연결된 선(Connection) 가져오기
-            connections = []
-            input_port.GetConnections(maxon.PORT_DIR.INPUT, connections)
-            for connection in connections:
-                source_port = connection[0] # 연결된 소스 포트 (출력 포트)
-                # RemoveConnection(source, destination)
-                maxon.GraphModelHelper.RemoveConnection(source_port, input_port)
-    break # 포트를 찾았으므로 루프 종료
-
-
-
-
 def GetAllObjects(doc):
     """
     씬 내의 모든 오브젝트를 리스트로 반환합니다.
@@ -126,103 +103,6 @@ def GetFullCache(objects, parent=True, deform=True, children=True) -> list:
                     _recurse(child_obj)
                     child_obj = child_obj.GetNext()
     return result_meshes
-
-TEXTURE_CHANNELS = {
-    "base_color":        [
-        "basecolor", "base", "color", "albedo", "diffuse", "diff", 
-        "col", "bc", "alb", "rgb" , "d"
-    ],
-    "normal":       [
-        "normal", "norm", "nrm", "nml", "nrml", "n" 
-    ],
-    "bump":         [
-        "bump", "b"
-    ],
-    "ao":           [
-        "ao", "ambient", "occlusion", "occ", "amb"
-    ],
-    "metalness":    [
-        "metallic", "metalness", "metal", "mtl", "met", "m"
-    ],
-    "refl_roughness":    [
-        "roughness", "rough", "rgh", "r"
-    ],
-    "refl_weight":     [
-        "specular", "spec", "s", "refl", "reflection"
-    ],
-    "glossiness":   [
-        "glossiness", "gloss", "g"
-    ],
-    "opacity_color":      [
-        "opacity", "opac", "alpha", "transparency", "transparent", 
-        "o", "a", "mask", "cutout" # 알파 마스크용 용어 추가
-    ],
-    "translucency": [
-        "translucency", "transmission", "trans", 
-        "sss", "subsurface", "scatter", "scattering" # SSS 관련 용어 보강
-    ],
-    "displacement": [
-        "displacement", "disp", "dsp", 
-        "height", "h"
-    ],
-    "emission_color":     [
-        "emissive", "emission", "emit", "illu", "illumination", "selfillum"
-    ]
-}
-
-def _split_into_components(fname):
-    """
-    Split filename into components with prefix filtering
-    'D_Wood_Maple_01_ROUGH_1.jpg' -> ['wood', 'maple', 'rough']
-    """
-    # Remove extension
-    fname = os.path.splitext(fname)[0]
-
-    # [NEW] Discard prefix: Keep string only after the LAST underscore
-    if "_" in fname:
-        # 마지막 _ 뒤의 부분만 가져옴
-        fname = fname.rsplit("_", 1)[-1]
-    else:
-        # 언더바가 없으면 조건에 맞지 않으므로 빈 리스트 반환
-        return []
-
-    # Remove digits
-    fname = "".join(i for i in fname if not i.isdigit())
-
-    # Separate CamelCase by space
-    fname = re.sub(r"([a-z])([A-Z])", r"\g<1> \g<2>", fname)
-
-    # Replace common separators with SPACE
-    separators = ["_", ".", "-", "__", "--", "#"]
-    for sep in separators:
-        fname = fname.replace(sep, " ")
-
-    components = fname.split(" ")
-    components = [c.lower() for c in components if c.strip()]
-    return components
-
-def GetTextureChannel(fname):
-    """
-    파일명의 마지막 '_' 뒤의 단어를 추출하여 채널을 판별합니다.
-    점수 계산 없이 정확히 일치하는 키워드가 있으면 해당 채널을 반환합니다.
-    """
-    # 1. 확장자 제거
-    base_name = os.path.splitext(fname)[0]
-    
-    # 2. '_'가 없으면 판별 불가
-    if "_" not in base_name:
-        return None
-        
-    # 3. 마지막 '_' 뒤의 단어 추출 및 소문자 변환
-    suffix = base_name.rsplit("_", 1)[-1].lower()
-    
-    # 4. 채널 매칭 확인
-    for channel, keywords in TEXTURE_CHANNELS.items():
-        if suffix in keywords:
-            return channel
-            
-    return None
-
 
 def GetMergedObject(self, op, doc): #Deprecated
     """Create a merged clone of all input objects in a dummy document and return that merged object."""
